8066028bcccd20ddb75962cf460907d2ad0a87b2
hadeoh <usthmandanfodio@gmail.com>
2020-01-30 23:34:29 -0800

    Write prepared queries for reusability

---
+++ src/queries/query.js
@@ -0,0 +1,29 @@
+// generic query builder for all models
+
+class Query {
+  constructor(Model) {
+    this.Model = Model;
+  }
+  
+  create(payload) {
+    return this.Model.create(payload);
+  }
+
+  findOne(payload) {
+    return this.Model.findOne(payload).exec();
+  }
+
+  findAll(payload) {
+    return this.Model.find(payload);
+  }
+
+  update({ payload, where }) {
+    return this.Model.findOneAndUpdate(where, payload, { new: true }).exec();
+  }
+
+  delete(payload) {
+    return this.Model.findOneAndDelete(payload).exec();
+  }
+}
+
+export default Query;
