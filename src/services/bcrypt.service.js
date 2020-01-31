f5a93b39b2d0a140910549fcc42a40296e944618
hadeoh <usthmandanfodio@gmail.com>
2020-01-31 15:25:27 -0800

    Implement hashing of password

---
+++ src/services/bcrypt.service.js
@@ -0,0 +1,9 @@
+import bcrypt from "bcrypt";
+import config from "../config";
+
+export const hashPassword = user => {
+  const hash = bcrypt.genSaltSync(config.bcryptSalt);
+  return bcrypt.hash(user.password, hash);
+};
+
+export const comparePassword = (password, hash) => bcrypt.compare(password, hash);
