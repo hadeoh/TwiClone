e5830126f12ecebe8571b8ddc4af14fd9dd65f7b
hadeoh <usthmandanfodio@gmail.com>
2020-02-01 14:15:49 -0800

    Implement the route to be communicated to for replying a tweet

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -2,16 +2,17 @@ import { Router } from 'express';
 import { celebrate as validate } from 'celebrate';
 import paramsValidation from '../validations/tweet.validation';
 import * as tweetController from '../controllers/tweeet.controller';
-import { auth as Auth} from '../policies/auth.policy'
+import { auth as Auth } from '../policies/auth.policy';
 
 const router = Router();
 
 router.use(Auth);
 router
   .route('/')
-  .post(
-    validate(paramsValidation.postTweet, { abortEarly: false }),
-    tweetController.postTweet
-  );
+  .post(validate(paramsValidation.postTweet, { abortEarly: false }), tweetController.postTweet);
+
+router
+  .route('/:tweetId/reply')
+  .post(validate(paramsValidation.tweetReply, { abortEarly: false }), tweetController.replyTweet);
 
 export default router;
