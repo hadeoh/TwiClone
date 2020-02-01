9f027ab3660462e4824adf1cb61b21e9a282dca7
hadeoh <usthmandanfodio@gmail.com>
2020-02-01 01:13:57 -0800

    Import all necessary files and make a call to the controller

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,14 +2,25 @@ import { Router } from 'express';
 import { celebrate as validate } from 'celebrate';
 import paramsValidation from '../validations/auth.validation';
 import validateSignUp from '../middlewares/signup.middleware';
+import validateLogin from '../middlewares/login.middleware';
 import * as authController from '../controllers/auth.controller';
 
 const router = Router();
 
-router.route('/signup').post(
-  validate(paramsValidation.signUp, { abortEarly: false }),
-  validateSignUp,
-  authController.signUp
-);
+router
+  .route('/signup')
+  .post(
+    validate(paramsValidation.signUp, { abortEarly: false }),
+    validateSignUp,
+    authController.signUp
+  );
+
+router
+  .route('/login')
+  .post(
+    validate(paramsValidation.login, { abortEarly: false }),
+    validateLogin,
+    authController.login
+  );
 
 export default router;
