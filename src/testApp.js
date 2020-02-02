f6f753185e5cc02f1223488116427efbe1a73ecb
hadeoh <usthmandanfodio@gmail.com>
2020-02-02 01:19:47 -0800

    Create entry point file for tests

--- src/testApp.js
+++ src/testApp.js
@@ -4,14 +4,10 @@
 // import '@babel/polyfill';
 import bodyParser from 'body-parser';
 import express from 'express';
-import cors from 'cors';
-import logger from 'morgan';
-import cookieParser from 'cookie-parser';
 
 /**
  * server configuration
  */
-import config from './config';
 import routes from './routes';
 import * as error from './config/error';
 
@@ -20,20 +16,9 @@ import * as error from './config/error';
  */
 const app = express();
 
-// allow cross origin requests
-// configure to only allow requests from certain origins
-app.use(cors({ credentials: true, origin: true }));
-
-app.use(cookieParser());
-
 // parsing the request body
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// enable detailed API logging in dev env
-if (config.env === 'development') {
-  app.use(logger('dev'));
-}
 
 // mount all routes on root /api/v1 path
 app.use('/api/v1', routes);
