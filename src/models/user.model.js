09ba6a1632df3fd63a50c5f68cab9242c47d7807
hadeoh <usthmandanfodio@gmail.com>
2020-02-03 10:43:26 -0800

    Modify the user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -14,7 +14,6 @@ const UserSchema = new Schema(
     },
     email: {
       type: String,
-      index: true,
       unique: true,
       lowercase: true,
       trim: true,
@@ -24,7 +23,6 @@ const UserSchema = new Schema(
     },
     phone: {
       type: String,
-      index: true,
       unique: true,
       trim: true,
       minlength: 6,
@@ -33,6 +31,7 @@ const UserSchema = new Schema(
     },
     userName: {
       type: String,
+      index: true,
       unique: true,
       lowercase: true,
       trim: true,
