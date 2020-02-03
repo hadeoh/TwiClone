670fe8cbade1a21d88687d8261e6dc93196156fd
hadeoh <usthmandanfodio@gmail.com>
2020-02-03 14:54:14 -0800

    Create controller for following user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,13 +2,17 @@ import httpStatus from 'http-status';
 import sendResponse from '../helpers/response';
 import { UserQuery } from '../queries';
 import * as bcryptService from '../services/bcrypt.service';
-import * as authService from '../services/auth.service'
+import * as authService from '../services/auth.service';
 
 export const signUp = async (req, res, next) => {
   try {
     const { fullName, email, phone, userName, password } = req.body;
 
-    const userExist = await UserQuery.findOne({ $or: [{ phone }, { email }, { userName }] });
+    const userExist = await UserQuery.findOne({
+      phone: { $eq: phone },
+      email: { $eq: email },
+      userName: { $eq: userName }
+    });
 
     if (userExist) {
       return res.status(httpStatus.BAD_REQUEST).json(
@@ -36,12 +40,14 @@ export const login = async (req, res, next) => {
   try {
     const { loginParams, password } = req.body;
 
-    let user = await UserQuery.findOne({ $or: [{ phone: loginParams }, { email: loginParams }, { userName: loginParams }] })
+    let user = await UserQuery.findOne({
+      $or: [{ phone: loginParams }, { email: loginParams }, { userName: loginParams }]
+    });
 
     if (!user) {
       return res.status(httpStatus.NOT_FOUND).json(
-        sendResponse(httpStatus.NOT_FOUND, "User does not exist", null, {
-          error: "User does not exist"
+        sendResponse(httpStatus.NOT_FOUND, 'User does not exist', null, {
+          error: 'User does not exist'
         })
       );
     }
@@ -50,16 +56,14 @@ export const login = async (req, res, next) => {
       // to issue token with the user object, convert it to JSON
       const token = authService.issue(user.toJSON());
 
-      user = {user,token}
+      user = { user, token };
 
-      return res.json(
-        sendResponse(httpStatus.OK, "success", user, null)
-      );
+      return res.json(sendResponse(httpStatus.OK, 'success', user, null));
     }
 
     return res.status(httpStatus.BAD_REQUEST).json(
-      sendResponse(httpStatus.BAD_REQUEST, "invalid email/username/password or password", null, {
-        issue: "invalid email/username/password or password"
+      sendResponse(httpStatus.BAD_REQUEST, 'invalid email/username/password or password', null, {
+        issue: 'invalid email/username/password or password'
       })
     );
   } catch (err) {
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,31 @@
 import httpStatus from 'http-status';
-import { UserQuery } from '../queries';
+import { FollowQuery } from '../queries';
 import sendResponse from '../helpers/response';
 
 export const getAll = async (_req, res, next) => {
   try {
     const users = await UserQuery.findAll({});
     return res.json(sendResponse(httpStatus.OK, 'success', users, null));
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const followUser = async (req, res, next) => {
+  try {
+    const { id: user } = req.token;
+    const { followId } = req.params;
+
+    const unfollowUser = await FollowQuery.delete({ followId });
+
+    if (unfollowUser) {
+      return res.json(
+        sendResponse(httpStatus.OK, 'success', { status: 'User successfully unfollowed' }, null)
+      );
+    }
+
+    const follow = await FollowQuery.create({ user, followId });
+    return res.json(sendResponse(httpStatus.OK, 'success', follow, null));
   } catch (err) {
     next(err);
   }
